84344c457287d7caf8b5562ddbb6837cd2bb56aa
Kris <kriss_tinkaa@ukr.net>
2021-05-31 21:12:53 +0300

    add moon function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,26 @@
-let searchText = '';
-const url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ecb1fb8bfb1422c9eb54f8bf56eb52&query=';
-const urlMoonPhase = 'http://api.farmsense.net/v1/moonphases/?d=';
-const urlImg = 'https://image.tmdb.org/t/p/w300';
-const form = document.getElementsByClassName('form')[0];
-const searchBtn = document.getElementsByClassName('searchBtn')[0];
-const searchTerm = document.getElementsByClassName('searchTerm')[0]
-const filmCard = document.getElementsByClassName('filmCard')[0];
-let mainContent = '',
-    main = document.querySelector('#films');
+window.onload = function () {
+
+  let searchURL = '';
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ecb1fb8bfb1422c9eb54f8bf56eb52&query=';
+  const urlMoonPhase = 'http://api.farmsense.net/v1/moonphases/?d=';
+
+  const form = document.querySelector('form');
+  const searchBtn = document.querySelector('.searchBtn');
+  const searchTerm = document.querySelector('.searchTerm');
+  const filmCard = document.querySelector('.filmCard');
+  let mainContent = '';
 
 
-  form.addEventListener('submit', function(e){
+  searchBtn.onclick = function (e) {
     e.preventDefault();
-    searchText = searchTerm.value;
-    Movies(searchText);
-    console.log(searchText);
-    
-  });
+    let value = searchTerm.value
+    searchURL = url + value + '&page=1';
+    getMovies(searchURL);
+    console.log(searchURL);
+  }
 
-    function Movies(searchText){
-      fetch(url + searchText + '&page=1')
+  function getMovies(url){
+    fetch(url)
       .then(response => {
         if(response.status != 200){
             console.log('No problem!');
@@ -27,61 +28,51 @@ let mainContent = '',
           }
           return response.json();
       })
-      .then(films =>{
-        films.results.forEach(film => {
-          mainContent+=`
-          <div class="col-lg-4 col-md-6 col-sm-1 film-heroku my-3">
-            <div class="card">
-            <div class="card-header text-light">
-              <div class="card-title">
-                <h2>${film.original_title}</h2>
-                <h4>Release date: ${film.release_date}</h4>
-              </div>
-            </div>
-            <div class="card-body" data-id="${film.id}">
-              <img src="https://image.tmdb.org/t/p/w300${film.poster_path}">
-              <div class="card-text"></div>
-             </div>
-            </div>
-          </div>`;
-          });
-          console.log(films);
-          main.innerHTML = mainContent;
+      .then((films) => {
+        showMovies(films)
       })
-    }
+  }
+  function showMovies(films){
+    films.results.forEach(film => {
+      var timeMoon = Date.parse(`${film.release_date}`);
+      var timeMoonUrl = urlMoonPhase + timeMoon;
+      console.log(timeMoonUrl)
+      mainContent+=`
+      <div class="col-lg-4 col-md-6 col-sm-1 film-heroku my-3">
+        <div class="card">
+        <div class="card-header text-light">
+          <div class="card-title">
+            <h2>${film.original_title}</h2>
+            <h4>Release date: ${film.release_date}</h4>
+          </div>
+        </div>
+        <div class="card-body" data-id="${film.id}">
+          <img src="https://image.tmdb.org/t/p/w300${film.poster_path}">
+          <div class="card-moonPhase">1</div>
+          </div>
+        </div>
+      </div>`;
+      });
+      console.log(films);
+      main.innerHTML = mainContent;
+      getMoon(timeMoonUrl);
+  }
+};
 
-    //Movies('car');
-//
-// function getMovies(searchText){
-//   fetch(url + searchText + '&page=1')
-//     .then(function (response) {
-//       console.log(url + searchText + '&page=1')
-//       console.log(response)
-//       if(response.status != 200){
-//         console.log('No problem!');
-//         return;
-//       }
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log('data', data)
-//       //SetMovies(data)
-//     })
-//     .catch(function (error) {
-//       console.log('error', error)
-//     })
-// }
-
-// function SetMovies(data){
-//   const card = document.getElementsByClassName('card');
-//   const cardImg = document.getElementsByClassName('card__img');
-//   const cardName = document.getElementsByClassName('card__name');
-//   const cardTime = document.getElementsByClassName('card__time');
-//   data.forEach(films => {
-
-//   })
-// }
-
-
-
+  function getMoon(timeMoon){
+    fetch('urlMoonPhase + timeMoon')
+    .then(response => {
+        if (response.status != 200) {
+          console.log('No problem!');
+          return;
+        }
+        return response.json();
+      })
+      .then(data => {
+        data.forEach(element => {
+          const moonPhase = document.querySelector('.card-moonPhase');
+          moonPhase.innerHTML += 'Moon: ' + `${element.Phase}`;
+        })
+      })
+  }
 
