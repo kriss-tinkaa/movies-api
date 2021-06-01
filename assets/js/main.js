de96ea0ba55e448664558df9f2e10824f704ce8d
kradchenkonlt <k.radchenko@newlinetechnologies.net>
2021-06-01 16:06:42 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,41 @@
 window.onload = function () {
 
-  let searchURL = '';
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ecb1fb8bfb1422c9eb54f8bf56eb52&query=';
-  const urlMoonPhase = 'http://api.farmsense.net/v1/moonphases/?d=';
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ecb1fb8bfb1422c9eb54f8bf56eb52&query=',
+        urlMoonPhase = 'http://api.farmsense.net/v1/moonphases/?d=',
+        searchBtn = document.querySelector('.searchBtn'),
+        searchTerm = document.querySelector('.searchTerm'),
+        filmCard = document.querySelector('.filmCard');
 
-  const form = document.querySelector('form');
-  const searchBtn = document.querySelector('.searchBtn');
-  const searchTerm = document.querySelector('.searchTerm');
-  const filmCard = document.querySelector('.filmCard');
-  let mainContent = '';
-  let main = document.querySelector('#films');
-  let timeMoonURL;
-  let timeMoon;
+  let   searchURL = '',
+        mainContent = '',
+        main = document.querySelector('#films'),
+        timeMoon;
 
 
   searchBtn.onclick = function (e) {
     e.preventDefault();
-    let value = searchTerm.value
+    let value = searchTerm.value;
     searchURL = url + value + '&page=1';
     getMovies(searchURL);
-    console.log(searchURL);
   }
 
-  function getMovies(url){
-    fetch(url)
-      .then(response => {
-        if(response.status != 200){
-            console.log('No problem!');
-            return;
-          }
-          return response.json();
-      })
-      .then((films) => {
-        showMovies(films)
-      })
-  }
-  function showMovies(films){
+
+  let getMovies = function getMovies(url){
+      fetch(url)
+        .then(response => {
+          if(response.status != 200){
+              console.log('No problem!');
+              return;
+            }
+            return response.json();
+        })
+        .then((films) => {
+          showMovies(films)
+        })
+    }
+
+let showMovies = function showMovies(films){
     films.results.forEach(film => {
-      timeMoon = Date.parse(`${film.release_date}`);
-      timeMoonURL = urlMoonPhase + timeMoon;
-      console.log(timeMoonURL)
       mainContent+=`
       <div class="col-lg-4 col-md-6 col-sm-1 film-heroku my-3">
         <div class="card">
@@ -55,15 +51,15 @@ window.onload = function () {
           </div>
         </div>
       </div>`;
-      });
-      console.log(films);
-      main.innerHTML = mainContent;
-      getMoon(timeMoonURL);
+      timeMoon = Date.parse(`${film.release_date}`);
+      getMoon(timeMoon, filmCard);
+    });
+    main.innerHTML = mainContent;
   }
 };
 
-  function getMoon(timeMoonURL){
-    fetch(timeMoonURL)
+  function getMoon(timeMoon){
+    fetch(`http://api.farmsense.net/v1/moonphases/?d=${timeMoon}`)
     .then(response => {
         if (response.status != 200) {
           console.log('No problem!');
@@ -71,11 +67,17 @@ window.onload = function () {
         }
         return response.json();
       })
-      .then(data => {
-        data.forEach(element => {
-          let moonPhase = document.querySelector('.card-moonPhase');
-          moonPhase.innerHTML += 'Moon: ' + `${element.Phase}`;
-        })
-      })
+      .then((data) =>{
+        showMoon(data)
+        console.log(`http://api.farmsense.net/v1/moonphases/?d=${timeMoon}`)
+       })
+  }
+
+  function showMoon(data){
+    data.forEach(element =>{
+
+      const moonPhase = document.querySelector('.card-moonPhase');
+      moonPhase.innerHTML = 'Phase Moon: ' + `${element.Phase}`;
+    })
   }
 
