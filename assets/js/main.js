683c953b04b1beeded837275e842f4a64c7c758e
kradchenkonlt <k.radchenko@newlinetechnologies.net>
2021-06-01 17:43:34 +0300

    change showMovies

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,7 @@ window.onload = function () {
   const url = 'https://api.themoviedb.org/3/search/movie?api_key=f4ecb1fb8bfb1422c9eb54f8bf56eb52&query=',
         urlMoonPhase = 'http://api.farmsense.net/v1/moonphases/?d=',
         searchBtn = document.querySelector('.searchBtn'),
-        searchTerm = document.querySelector('.searchTerm'),
-        filmCard = document.querySelector('.filmCard');
+        searchTerm = document.querySelector('.searchTerm');
 
   let   searchURL = '',
         mainContent = '',
@@ -34,50 +33,53 @@ window.onload = function () {
         })
     }
 
-let showMovies = function showMovies(films){
-    films.results.forEach(film => {
-      mainContent+=`
-      <div class="col-lg-4 col-md-6 col-sm-1 film-heroku my-3">
-        <div class="card">
-        <div class="card-header text-light">
-          <div class="card-title">
-            <h2>${film.original_title}</h2>
-            <h4>Release date: ${film.release_date}</h4>
-          </div>
-        </div>
-        <div class="card-body" data-id="${film.id}">
-          <img src="https://image.tmdb.org/t/p/w300${film.poster_path}">
-          <div class="card-moonPhase">1</div>
-          </div>
-        </div>
-      </div>`;
-      timeMoon = Date.parse(`${film.release_date}`);
-      getMoon(timeMoon, filmCard);
-    });
-    main.innerHTML = mainContent;
-  }
-};
+    let showMovies = function showMovies(films){
+      films.results.forEach(film => {
+        const CardBlock = document.createElement('div');
+        const image = document.createElement('img');
+        const title = document.createElement('h2');
+        const realese = document.createElement('p');
+        const timeMoon = Date.parse(`${film.release_date}`);
 
-  function getMoon(timeMoon){
-    fetch(`http://api.farmsense.net/v1/moonphases/?d=${timeMoon}`)
-    .then(response => {
-        if (response.status != 200) {
-          console.log('No problem!');
-          return;
-        }
-        return response.json();
-      })
-      .then((data) =>{
-        showMoon(data)
-        console.log(`http://api.farmsense.net/v1/moonphases/?d=${timeMoon}`)
-       })
-  }
+        CardBlock.classList.add('card', 'col-lg-4', 'col-md-6', 'col-sm-1', 'my-3');
+        title.classList.add('card-title')
+        
+        title.innerHTML =`${film.original_title}`;
+        image.src = `https://image.tmdb.org/t/p/w300${film.poster_path}`;
+        realese.innerHTML = `Release date: ${film.release_date}`;
 
-  function showMoon(data){
-    data.forEach(element =>{
+        CardBlock.appendChild(title);
+        CardBlock.appendChild(image);
+        CardBlock.appendChild(realese);
+        main.appendChild(CardBlock);
 
-      const moonPhase = document.querySelector('.card-moonPhase');
-      moonPhase.innerHTML = 'Phase Moon: ' + `${element.Phase}`;
+        getMoonPhase(timeMoon, CardBlock);
+
+
+      });
+      console.log(timeMoon);
+
+    }
+  };
+
+  let getMoon = function getMoon(timeMoon, CardBlock){
+      fetch(`http://api.farmsense.net/v1/moonphases/?d=${timeMoon}`)
+      .then(response => {
+          if (response.status != 200) {
+            console.log('No problem!');
+            return;
+          }
+          return response.json();
+        })
+        .then((data) =>{
+          showMoon(data, CardBlock)
+        })
+    }
+
+  let showMoon = function showMoon(data, CardBlock){
+    data.forEach(element =>{ 
+      const moonPhaseBlock = `<div class="card-phaseMoon" data-id="Phase: ${element.Phase}">`;
+      CardBlock.appendChild(moonPhaseBlock);
     })
   }
 
